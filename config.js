a4364edd35bfe975d0baa13dabfc2119bfe4f1f0
Tom Foglio <tomcfoglio@users.noreply.github.com>
2017-07-18 11:58:41 +0100

    bug fix

--- config.js
+++ config.js
@@ -14,7 +14,6 @@ exports = module.exports = {
         }
     },
     queue: {
-<<<<<<< HEAD
         uri: process.env.CLDK_WORKER_USER_QUEUE_URI || 'amqps://mq.dev.cloudoki.com',
         reconnect: process.env.CLDK_WORKER_USER_QUEUE_RECONNECT || 5000,
         options: {
@@ -22,15 +21,6 @@ exports = module.exports = {
             key: process.env.CLDK_WORKER_USER_QUEUE_KEY || '/Users/tomasfoglio/Cloudoki/donderstarter-api/ssl/client/key.pem',
             passphrase: process.env.CLDK_WORKER_USER_QUEUE_CERT_PASS || 'cloudoki',
             ca: process.env.CLDK_WORKER_USER_QUEUE_CA || '/Users/tomasfoglio/Cloudoki/donderstarter-api/ssl/ca/cacert.pem'
-=======
-        uri: process.env.CLDK_WORKER_QUEUE_URI || 'amqps://mq.dev.cloudoki.com',
-        reconnect: process.env.CLDK_WORKER_QUEUE_RECONNECT || 5000,
-        options: {
-            cert: process.env.CLDK_WORKER_QUEUE_CERT || 'ssl/cert.pem',
-            key: process.env.CLDK_WORKER_QUEUE_KEY || 'ssl/key.pem',
-            passphrase: process.env.CLDK_WORKER_QUEUE_CERT_PASS || 'cloudoki',
-            ca: process.env.CLDK_WORKER_QUEUE_CA || 'ssl/cacert.pem'
->>>>>>> 0814759e7fce422d6138aa6e8df1a721cee4f53a
         }
     }
 };
--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const config = require('config');
 const log = require('log');
 const mq = require('hyper-queue');
 
-<<<<<<< HEAD
 const config = require('config');
 const log = require('log');
 
 const user = require('lib/consumers/user');
-=======
-const placeholder = require('lib/consumers/placeholder');
-
-mq.logger(log);
->>>>>>> 0814759e7fce422d6138aa6e8df1a721cee4f53a
 
 const account = require('lib/consumers/account');
 
@@ -21,7 +15,6 @@ const role = require('lib/consumers/role');
 
 const permission = require('lib/consumers/permission');
 
-<<<<<<< HEAD
 mq.logger(log);
 
 mq.broker(config.queue.uri, config.queue.options, config.queue.reconnect);
@@ -33,8 +26,5 @@ mq.registerConsumers(account.consumers);
 mq.registerConsumers(role.consumers);
 
 mq.registerConsumers(permission.consumers);
-=======
-mq.registerCconsumers(placeholder.consumers);
->>>>>>> 0814759e7fce422d6138aa6e8df1a721cee4f53a
 
 mq.connect();
