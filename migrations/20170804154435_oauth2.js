55fb64851203de245e5d7b3fb659dd3dac170ee1
a31859 <a31859@users.noreply.github.com>
2017-08-07 11:22:25 +0100

    update:
    add missing certificates
    add trusted attribute to client and trusted validation
    add authorization dialog verification

--- migrations/20170804154435_oauth2.js
+++ migrations/20170804154435_oauth2.js
@@ -8,6 +8,7 @@ exports.up = function (knex, Promise) {
 			table.string('name');
 			table.string('description');
 			table.string('grant_types');
+			table.boolean('trusted');
 			table.string('scope');
 			table.integer('user_id').unsigned().references('user.id');
 			table.timestamps();
